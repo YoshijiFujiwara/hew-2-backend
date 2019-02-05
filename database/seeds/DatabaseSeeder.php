dcce8ac804e508d162a1623e5476fcbffff16d33
yoshiji <yfbiology2@gmail.com>
2019-02-05 23:21:08 +0900

    id1を固定のテストユーザーとして作成する

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -60,6 +60,13 @@ class DatabaseSeeder extends Seeder
                 });
         });
 
+        // id 1番を固定のユーザーとする
+        $testUser = \App\User::find(1);
+        $testUser->email = 'testuser@example.com';
+        $testUser->name = 'テスト太郎';
+        $testUser->username = 'testuser';
+        $testUser->save();
+
 
 //        factory(\App\User::class, 30)
 //            ->create()
