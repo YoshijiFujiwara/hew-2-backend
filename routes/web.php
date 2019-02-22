25ce1256af29af1928920ae03042e14c303dd1f2
Tsukasa Murata <stepfootprint@gmail.com>
2019-02-22 23:26:10 +0900

    push通知デプロイ

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/tasks/queue', function () {
+    Artisan::call('queue:work',['--stop-when-empty' => true]);
+    return response('Queue started.', 200)->header('Content-Type', 'text/plain');
+})->name('tasks.queue');
