7015d939414485565c63366489fd681e2579986f
yoshiji <yfbiology2@gmail.com>
2019-02-23 01:46:41 +0900

    attribute event

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,4 +16,8 @@ class Controller extends BaseController
     protected const USER_CREATE_EVENT = 'user_create';
     protected const USER_UPDATE_EVENT = 'user_update';
     protected const USER_DELETE_EVENT = 'user_delete';
+
+    protected const ATTRIBUTE_CREATE_EVENT = 'attribute_create';
+    protected const ATTRIBUTE_UPDATE_EVENT = 'attribute_update';
+    protected const ATTRIBUTE_DELETE_EVENT = 'attribute_delete';
 }
