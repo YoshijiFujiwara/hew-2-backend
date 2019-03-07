f866d90b4afe2769b6cb2e6fa463527b8dab1c76
yoshiji <yfbiology2@gmail.com>
2019-03-07 18:26:14 +0900

    guest session のとき、自分の予算を返す

--- app/Http/Controllers/Guest/SessionController.php
+++ app/Http/Controllers/Guest/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Http\Resources\GuestSessionResource;
 use App\Http\Resources\SessionResource;
 use App\Http\Resources\UserResource;
 use App\Jobs\PushNotification;
@@ -27,7 +28,58 @@ class SessionController extends Controller
      */
     public function index(Request $request)
     {
-        return SessionResource::collection($request->user()->participatedSessions);
+        $guestSessions = collect([]);
+
+        foreach ($request->user()->participatedSessions as $session) {
+            $mySessionUserInfo = $session->users()->where('id', $request->user()->id)->first();
+            $myPlusMinus = (isset($mySessionUserInfo->pivot->plus_minus))? $mySessionUserInfo->pivot->plus_minus: 0;
+
+            $myBudgetEstimate = 0; // 自身の支払い予定金額
+            $myBudgetActual = 0; // 自身の支払い決定金額
+
+//            Log::debug(print_r($mySessionUserInfo->pivot->join_status, true));
+
+            $managerCost = 0;
+            // 予定金額の方
+            if ($session->budget != null && $session->budget > 0 && $mySessionUserInfo->pivot->join_status == 'allow') {
+                $allowUserCount = $session->users()->wherePivot('join_status', 'allow')->count();
+//                Log::debug('------');
+//                Log::debug('sessionId'. $session->id);
+//                Log::debug('allowUser' . $allowUserCount);
+                $sum = $session->budget * $allowUserCount;
+                foreach ($session->users()->wherePivot('join_status', 'allow')->get() as $allowUser) {
+                    $sum -= isset($allowUser->pivot->plus_minus)? $allowUser->pivot->plus_minus : 0;
+                }
+                $managerCost = ((int)($sum / $session->unit_rounding_budget) / ($allowUserCount + 1)) * $session->unit_rounding_budget;
+//                Log::debug('managerconst'. $managerCost);
+                $myBudgetEstimate = $managerCost + $myPlusMinus;
+            }
+
+            $managerCost = 0;
+            // 支払い決定金額の方
+            if ($session->actual != null && $session->actual > 0 && $mySessionUserInfo->pivot->join_status == 'allow') {
+
+                $allowUserCount = $session->users()->wherePivot('join_status', 'allow')->count();
+                $sum = $session->actual * $allowUserCount;
+                foreach ($session->users()->wherePivot('join_status', 'allow')->get() as $allowUser) {
+                    $sum -= isset($allowUser->pivot->plus_minus)? $allowUser->pivot->plus_minus : 0;
+                }
+                $managerCost = ((int)($sum / $session->unit_rounding_actual) / ($allowUserCount + 1)) * $session->unit_rounding_actual;
+//                Log::debug('managerconst'. $managerCost);
+
+                $myBudgetActual = $managerCost + $myPlusMinus;
+            }
+//            Log::debug('myestimate');
+//            Log::debug($myBudgetEstimate);
+//            Log::debug('myactual');
+//            Log::debug($myBudgetActual);
+            $session->my_budget_estimate = $myBudgetEstimate;
+            $session->my_budget_actual = $myBudgetActual;
+
+            $guestSessions->push($session);
+        }
+
+        return GuestSessionResource::collection($guestSessions);
     }
 
     /**
