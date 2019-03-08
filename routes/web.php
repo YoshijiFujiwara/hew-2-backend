63a5e507ac90682de2d9daf66537d1dd0b10492e
Tsukasa Murata <stepfootprint@gmail.com>
2019-03-09 01:33:05 +0900

    cron用ルート追加

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ Route::get('/tasks/queue', function () {
     Artisan::call('queue:work',['--stop-when-empty' => true]);
     return response('Queue started.', 200)->header('Content-Type', 'text/plain');
 })->name('tasks.queue');
+
+Route::get('/tasks/shopInfo', function () {
+    Artisan::call('make:shopinfo');
+    return response('Make shopInfo started.', 200)->header('Content-Type', 'text/plain');
+})->name('tasks.shopInfo');
