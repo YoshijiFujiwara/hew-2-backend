fe3302d52558142f0d469940016dd80595203a45
yoshiji <yfbiology2@gmail.com>
2019-03-08 23:55:57 +0900

    shopresource

---
+++ app/Http/Resources/ShopResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShopResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'shop_id' => $this->shop_id,
+            'name' => $this->name,
+            'lng' => $this->lng,
+            'lat' => $this->lat,
+            'genre_id' => $this->genre_id,
+            'genre_name' => $this->genre_name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
