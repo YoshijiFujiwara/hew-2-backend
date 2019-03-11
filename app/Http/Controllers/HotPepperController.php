8aadba11037965fbe363234b7f99bb48de7d6bd1
Tsukasa Murata <stepfootprint@gmail.com>
2019-03-11 22:04:21 +0900

    おすすめAPI返却値変更

--- app/Http/Controllers/HotPepperController.php
+++ app/Http/Controllers/HotPepperController.php
@@ -18,7 +18,8 @@ class HotPepperController extends Controller
      * hotpepper.search.favorite ホットペッパーの検索の際に、店IDをおすすめ順に並べ替える。その中に自分のいったことのある店があれば、[1番人気, 2番人気, 行ったことがある]の順に返す。なければ、[1番人気, 2番人気, ３番人気]の順に返す。レスポンス速度があやしい。
      *
      * @bodyParam body 連想配列 required ホットペッパーの検索の際に使う、クエリパラメータをAPIKEYを除いて渡す
-     * @bodyParam count int 返却する店件数（指定なしの場合３件）
+     * @bodyParam count int おすすめ店件数（指定なしの場合３件）
+     * @bodyParam size int 返却する総件数（指定なしの場合３０件）
      *
      * @response {
      * "data": [
@@ -45,7 +46,6 @@ class HotPepperController extends Controller
             $response = $client->request('GET', $url);
             $result = json_decode($response->getBody());
             $shopInfos = $result->results->shop;
-
             $shopIds = [];
             // idを抽出する
             foreach ($shopInfos as $shopInfo) {
@@ -89,36 +89,47 @@ class HotPepperController extends Controller
 //                }
 //            }
 
+            // おすすめ店舗情報取得
             $i = 0;
-            $size = 3;
-            if ($request->has('count')){
-                $size = $request->count;
+            $count = 3;
+            if ($request->has('count')) {
+                $count = $request->count;
             }
+            $size = 30;
+            if ($request->has('size')) {
+                $size = $request->size;
+            }
+
             foreach ($recommendIds as $id) {
                 $i++;
-                if ($i > $size) {
+                if ($i > $count) {
                     break;
                 }
                 $resultArray[] = $id;
             }
-            // 件数が少ない場合素の検索結果から補填
-            foreach ($shopInfos as $shopInfo) {
-                $i++;
-                if ($i > $size) {
+
+            // 残りは上から適当に
+            $result = [];
+            // 店情報取得
+            foreach ($resultArray as $id) {
+                $result[] = array_first($shopInfos, function ($value, $key) use ($id) {
+                    return $value->id == $id;
+                });
+            }
+            foreach ($shopInfos as $shop) {
+                if (array_first($result, function ($value, $key) use ($shop) {
+                    return $value->id == $shop->id;
+                },true) === true) {
+                    $result[] = $shop;
+                }
+                if (count($result) >= $size) {
                     break;
                 }
-                // 既に存在するSHOPなら飛ばす
-                if (in_array($shopInfo->id, $resultArray)) {
-                    $i--;
-                    continue;
-                } else {
-                    $resultArray[] = $shopInfo->id;
-                }
             }
-            // それで足りなければ諦める
+            $wrap["shop"] = $result;
 
             return response()->json([
-                'data' => $resultArray
+                'results' => $wrap
             ]);
 
         } catch (ClientException $exception) {
@@ -160,6 +171,6 @@ class HotPepperController extends Controller
             echo '\'';
             echo ',';
         }
-        return ;
+        return;
     }
 }
